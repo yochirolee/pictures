20adeb32d5b6cbe7d5dcc36da1491355e71e7160
yochirolee <yleecruz@gmail.com>
2020-05-27 19:42:30 -0500

    Updates

---
+++ precache-manifest.88e5d8e68badc578f4a7b2c152dd63cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aeb1c44de5922d4a4f832faa9e73fc8",
+    "url": "/pictures/index.html"
+  },
+  {
+    "revision": "786d5966e2a1ee9384e5",
+    "url": "/pictures/static/css/main.d917c751.chunk.css"
+  },
+  {
+    "revision": "757daafec3cfa3eb4332",
+    "url": "/pictures/static/js/2.79a78eae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pictures/static/js/2.79a78eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786d5966e2a1ee9384e5",
+    "url": "/pictures/static/js/main.741e8470.chunk.js"
+  },
+  {
+    "revision": "0cc7bc421b35eb0accf3",
+    "url": "/pictures/static/js/runtime-main.13e019b1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pictures/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pictures/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pictures/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pictures/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pictures/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
